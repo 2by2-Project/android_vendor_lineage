43e71bb2644d6ad3c8f6a7893a1b278264065a55
kailua <kailua@project2by2.jp>
2026-03-04 18:45:04 +0900

    config: common: Include 2by2 base configuration
    Change-Id: I61ce17199bc970501aba32babc90c666eee263e4

--- config/common.mk
+++ config/common.mk
@@ -276,3 +276,6 @@ include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+# Include 2by2 ROM base configurations.
+include vendor/2by2/config/common.mk
